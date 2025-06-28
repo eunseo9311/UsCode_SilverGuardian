3faa1f7523910f9f870b90711cc0364c8b5c595b
eunseo9311 <songeunseo@soongsil.ac.kr>
2025-06-29 00:38:12 +0900

    feat: 실버 가디언 이름 변경

--- apps/admin/src/components/topbar.tsx
+++ apps/admin/src/components/topbar.tsx
@@ -1,11 +1,11 @@
 // components/topbar.tsx
 'use client'
 
-import { Box, Flex, Text, Image } from '@devup-ui/react'
+import { Box, Flex, Image, Text } from '@devup-ui/react'
 
 export default function Topbar() {
   return (
-    <Box w="100%" h="100px" bg="#6F6F6F">
+    <Box bg="#6F6F6F" h="100px" w="100%">
       <Flex alignItems="center" h="100%" px="24px">
         {/* 빈 공간으로 밀어내기 */}
         <Box flex={1} />
@@ -15,17 +15,12 @@ export default function Topbar() {
           fontFamily="Pretendard"
           fontSize="32px"
           fontWeight="600"
-          lineHeight="1em"
           letterSpacing="0em"
+          lineHeight="1em"
         >
           환영합니다! 김의성님
         </Text>
-        <Image
-          src="/logo2.svg"
-          alt="Logo"
-          boxSize="64px"
-          h="64px"
-        />
+        <Image alt="Logo" boxSize="64px" h="64px" src="/logo2.svg" />
       </Flex>
     </Box>
   )
